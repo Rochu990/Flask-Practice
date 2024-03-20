314d8f25f8237b968666fb73e5fd8b944e004d8c
Przemek Rostek <rochu9900@gmail.com>
2024-03-20 21:04:49 +0100

    Flask - apka robot

--- Flask - aplikacja car/app.py
+++ Flask - aplikacja car/app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 import os
 
+from flask import Flask, jsonify, request
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "db.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 app.app_context().push()
+
 
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,7 +22,6 @@ class Car(db.Model):
     combustion = db.Column(db.Integer)
     tank_fuel = db.Column(db.Integer)
     max_fuel = db.Column(db.Integer)
-    
 
     def __init__(self, combustion: int, tank_fuel: int, max_fuel: int):
         self.combustion = combustion
@@ -31,29 +33,28 @@ class Car(db.Model):
         if self.tank_fuel > self.max_fuel:
             print("Próbujesz zatankować za dużo")
             self.tank_fuel = self.max_fuel
-        
-        
+
     def drive(self, km):
-        fuel_after_tank = self.tank_fuel
         fuel = (km / 100) * self.combustion
         self.tank_fuel = self.tank_fuel - fuel
         if self.tank_fuel < 0:
             self.tank_fuel = 0
-        
+
 
 class CarSchema(ma.Schema):
     class Meta:
         fields = ("id", "combustion", "tank_fuel", "max_fuel")
 
+
 car_schema = CarSchema()
 cars_schema = CarSchema(many=True)
 
 
-@app.route('/car', methods=['POST'])
+@app.route("/car", methods=["POST"])
 def add_car():
-    combustion = request.json['combustion']
-    tank_fuel = request.json['tank_fuel']
-    max_fuel = request.json['max_fuel']
+    combustion = request.json["combustion"]
+    tank_fuel = request.json["tank_fuel"]
+    max_fuel = request.json["max_fuel"]
 
     new_car = Car(combustion, tank_fuel, max_fuel)
 
@@ -62,12 +63,14 @@ def add_car():
 
     return car_schema.jsonify(new_car)
 
-@app.route('/car/<id>', methods=['GET'])
+
+@app.route("/car/<id>", methods=["GET"])
 def get_car(id):
     car = Car.query.get(id)
     return car_schema.jsonify(car)
 
-@app.route('/car/<id>', methods=['DELETE'])
+
+@app.route("/car/<id>", methods=["DELETE"])
 def delete_car(id):
     car = Car.query.get(id)
     db.session.delete(car)
@@ -75,27 +78,30 @@ def delete_car(id):
 
     return car_schema.jsonify(car)
 
-@app.route('/car/<id>/drive', methods=['POST'])
+
+@app.route("/car/<id>/drive", methods=["POST"])
 def drive(id):
-    
+
     car = Car.query.get(id)
-    km = request.json['km']
+    km = request.json["km"]
     car.drive(km)
     db.session.add(car)
     db.session.commit()
 
     return car_schema.jsonify(car)
 
-@app.route('/car/<id>/refuel', methods=['POST'])
+
+@app.route("/car/<id>/refuel", methods=["POST"])
 def refuel(id):
-    
+
     car = Car.query.get(id)
-    fuel = request.json['fuel']
+    fuel = request.json["fuel"]
     car.refuel(fuel)
     db.session.add(car)
     db.session.commit()
-    
+
     return car_schema.jsonify(car)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
